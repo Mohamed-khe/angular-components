907b44d13b64dd7d8eaa3a286ebf4855ceeb1864
tsteelematc <tsteele@madisoncollege.edu>
2026-02-28 11:36:37 -0600

    g generate component pizza-toppings --skip-tests

---
+++ src/app/pizza-toppings/pizza-toppings.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-toppings',
+  imports: [],
+  templateUrl: './pizza-toppings.html',
+  styleUrl: './pizza-toppings.css',
+})
+export class PizzaToppings {
+
+}
