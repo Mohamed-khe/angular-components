c2ed83e40882c155fc0d38c698f6ddf7870fed13
THamilton1123 <thamilton12@madisoncollege.edu>
2026-03-11 12:13:53 -0500

    Working, phew :-)

--- src/app/thamilton-roll-d20/thamilton-roll-d20.ts
+++ src/app/thamilton-roll-d20/thamilton-roll-d20.ts
@@ -16,4 +16,9 @@ export class ThamiltonRollD20 {
   protected readonly numberOne = signal(2);
   protected readonly numberTwo = signal(2);
   protected readonly sum = signal(4);
+
+  protected readonly updateNumberOne = (num: string) => this.numberOne.set(Number(num));
+  protected readonly updateNumberTwo = (num: string) => this.numberTwo.set(Number(num));
+
+  protected readonly add = () => this.sum.set(this.numberOne() + this.numberTwo());
 }
