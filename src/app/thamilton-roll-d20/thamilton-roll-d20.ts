4f1bc714c79cfc7ab1dc14063a3d891ed4d6840c
THamilton1123 <thamilton12@madisoncollege.edu>
2026-03-11 13:06:01 -0500

    Got Xd20 roller working :-)

--- src/app/thamilton-roll-d20/thamilton-roll-d20.ts
+++ src/app/thamilton-roll-d20/thamilton-roll-d20.ts
@@ -11,7 +11,21 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './thamilton-roll-d20.css',
 })
 export class ThamiltonRollD20 {
-  protected readonly rollD20 = () => {};
+  protected readonly multiplier = signal(0);
+  protected readonly result = signal(0);
+
+  protected readonly updateMultiplier = (num: string) => this.multiplier.set(Number(num));
+
+  protected readonly rollD20 = () => {
+    let sum = 0;
+
+    for (let i = 0; i < Number(this.multiplier()); i++) {
+      let roll = Math.floor(Math.random() * 20) + 1;
+      sum += roll;
+    }
+
+    this.result.set(sum);
+  };
 
   protected readonly numberOne = signal(2);
   protected readonly numberTwo = signal(2);
