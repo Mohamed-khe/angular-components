dcfa01a07d4ea09c1ba4a0aa8866e22ecaf99d20
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-03-15 15:08:14 -0500

    Generate component from command line ng generate component zsmuckerbryan-checklist --skip-tests- Checklist
    CREATE src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.css (0 bytes)
    CREATE src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.ts (253 bytes)
    CREATE src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.html (38 bytes)

---
+++ src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zsmuckerbryan-checklist',
+  imports: [],
+  templateUrl: './zsmuckerbryan-checklist.html',
+  styleUrl: './zsmuckerbryan-checklist.css',
+})
+export class ZsmuckerbryanChecklist {
+
+}
