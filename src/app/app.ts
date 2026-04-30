d43ac363c589f41a23e28a5520956c081b34c3d8
Shaun Khang <khaunx02@gmail.com>
2026-04-30 15:41:42 -0500

    Showing my component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { PizzaToppings } from './pizza-toppings/pizza-toppings';
+import { SkhangEightball } from './skhang-eightball/skhang-eightball';
 
 @Component({
   selector: 'app-root',
-  imports: [PizzaToppings],
+  imports: [PizzaToppings, SkhangEightball],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-components');
