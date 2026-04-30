c4d49b1204b73954d2beb2c8be27ae9b4283f701
Shaun Khang <khaunx02@gmail.com>
2026-04-30 15:51:54 -0500

    Styled input and buttons

--- src/app/skhang-eightball/skhang-eightball.ts
+++ src/app/skhang-eightball/skhang-eightball.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-skhang-eightball',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, MatFormField, MatInputModule],
   templateUrl: './skhang-eightball.html',
   styleUrl: './skhang-eightball.css',
 })
