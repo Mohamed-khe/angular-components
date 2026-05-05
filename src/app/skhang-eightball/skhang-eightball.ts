2721abf481c6da68d9691bee36fd5c61875cf79c
Shaun Khang <khaunx02@gmail.com>
2026-05-04 20:04:30 -0500

    implemented random answer for 8 ball
    used math.floor with math random to get a random answer from the answers array to display onto the page

--- src/app/skhang-eightball/skhang-eightball.ts
+++ src/app/skhang-eightball/skhang-eightball.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { iterator } from 'rxjs/internal/symbol/iterator';
 
 @Component({
   selector: 'app-skhang-eightball',
@@ -11,6 +12,32 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './skhang-eightball.css',
 })
 export class SkhangEightball {
-  protected readonly question = signal('testing456');
-  protected readonly answer = signal('testing123');
+  protected readonly question = signal('');
+  protected readonly answer = signal('');
+
+  protected readonly answers = [
+    'it is certain',
+    'it is decidely so',
+    'without a doubt',
+    'yes definitely',
+    'you may rely on it',
+    'as i see it, yes',
+    'most likely',
+    'outlook good',
+    'yes',
+    'signs point to yes',
+    'reply hazy, try again',
+    'ask again later',
+    'better not tell you now',
+    'cannot predict now',
+    'concentrate and ask again',
+    "don't count on it",
+    'my sources say no',
+    'outlook not so good',
+    'very doubtful',
+  ];
+
+  protected readonly randAnswer = () => {
+    this.answer.set(this.answers[Math.floor(Math.random() * this.answers.length)]);
+  };
 }
