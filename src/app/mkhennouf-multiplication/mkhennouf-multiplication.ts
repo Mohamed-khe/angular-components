8a785f1dee4b6dbdcba5f9fe9556a8cc1aa95d2b
Mohamed-khe <mkhennouf@madisoncollege.edu>
2026-05-11 23:05:58 -0500

    ng generate component mkhennouf-multiplication --skip-tests

---
+++ src/app/mkhennouf-multiplication/mkhennouf-multiplication.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mkhennouf-multiplication',
+  imports: [],
+  templateUrl: './mkhennouf-multiplication.html',
+  styleUrl: './mkhennouf-multiplication.css',
+})
+export class MkhennoufMultiplication {
+
+}
