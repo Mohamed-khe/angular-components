d83849461707f696c0eeaab84a2e46ac014c2458
Mohamed-khe <mkhennouf@madisoncollege.edu>
2026-05-11 23:09:56 -0500

    Showing my component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { PizzaToppings } from './pizza-toppings/pizza-toppings';
+import { MkhennoufMultiplication } from './mkhennouf-multiplication/mkhennouf-multiplication';
 
 @Component({
   selector: 'app-root',
-  imports: [PizzaToppings],
+  imports: [
+    PizzaToppings,
+    MkhennoufMultiplication
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
