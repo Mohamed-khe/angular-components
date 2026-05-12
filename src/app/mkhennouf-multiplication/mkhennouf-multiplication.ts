58d96716be6d13d79934da7c5f2ebac5b7bc264d
Mohamed-khe <mkhennouf@madisoncollege.edu>
2026-05-11 23:17:30 -0500

    Placeholders there...

--- src/app/mkhennouf-multiplication/mkhennouf-multiplication.ts
+++ src/app/mkhennouf-multiplication/mkhennouf-multiplication.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-mkhennouf-multiplication',
-  imports: [],
+  imports: [
+    MatCardModule,
+  ],
   templateUrl: './mkhennouf-multiplication.html',
   styleUrl: './mkhennouf-multiplication.css',
 })
