8cf48b0f956fbdb672c383e08e83d6b648c0c102
Mohamed-khe <mkhennouf@madisoncollege.edu>
2026-05-11 23:26:03 -0500

    Styled input and buttons

--- src/app/mkhennouf-multiplication/mkhennouf-multiplication.ts
+++ src/app/mkhennouf-multiplication/mkhennouf-multiplication.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-mkhennouf-multiplication',
   imports: [
     MatCardModule,
-  ],
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+],
   templateUrl: './mkhennouf-multiplication.html',
   styleUrl: './mkhennouf-multiplication.css',
 })
