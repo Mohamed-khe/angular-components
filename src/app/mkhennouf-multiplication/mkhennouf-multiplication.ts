d1adff4ee0fb48041d63f1c9e05e7e943c9af290
Mohamed-khe <mkhennouf@madisoncollege.edu>
2026-05-11 23:33:14 -0500

    Initialize numbers and product...

--- src/app/mkhennouf-multiplication/mkhennouf-multiplication.ts
+++ src/app/mkhennouf-multiplication/mkhennouf-multiplication.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { S } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-mkhennouf-multiplication',
@@ -16,5 +17,9 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './mkhennouf-multiplication.css',
 })
 export class MkhennoufMultiplication {
+
+  protected readonly numberOne = signal(3);
+  protected readonly numberTwo = signal(3);
+  protected readonly multiplicationResult = signal(9);
 
 }
